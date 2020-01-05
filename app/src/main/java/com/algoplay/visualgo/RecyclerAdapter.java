cd6a796988686a572dd1cc7401d9b15eacd4b2b1
Rakibul islam <rakibcuet16@gmail.com>
2020-01-05 13:25:13 +0600

    recyclerview added

--- app/src/main/java/com/algoplay/visualgo/RecyclerAdapter.java
+++ app/src/main/java/com/algoplay/visualgo/RecyclerAdapter.java
@@ -2,17 +2,23 @@ package com.algoplay.visualgo;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.algoplay.visualgo.ModelClass.Algorithm;
+
 import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.viewHolder> {
-  ArrayList<Al>
+   ArrayList<Algorithm>myalgorithms;
+
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        
+
         return null;
     }
 
@@ -23,10 +29,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.viewHo
 
     @Override
     public int getItemCount() {
-        return 0;
+         if (myalgorithms.isEmpty())
+             return 0;
+         else
+             return myalgorithms.size();
     }
 
     public class viewHolder extends RecyclerView.ViewHolder{
+        TextView algoname;
+        TextView algocreator;
 
         public viewHolder(@NonNull View itemView) {
             super(itemView);

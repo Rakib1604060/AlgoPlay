20859fbaec5554a0289051e68cc17f4cd956728a
Rakibul islam <rakibcuet16@gmail.com>
2020-01-05 13:10:30 +0600

    sidebar added

---
+++ app/src/main/java/com/algoplay/visualgo/ModelClass/Algorithm.java
@@ -0,0 +1,7 @@
+package com.algoplay.visualgo.ModelClass;
+
+public class Algorithm {
+    String algoname;
+    String algocreator;
+    String 
+}
---
+++ app/src/main/java/com/algoplay/visualgo/RecyclerAdapter.java
@@ -0,0 +1,36 @@
+package com.algoplay.visualgo;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.viewHolder> {
+  ArrayList<Al>
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+    }
+}

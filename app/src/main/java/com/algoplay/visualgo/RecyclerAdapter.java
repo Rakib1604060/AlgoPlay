05d7ecb556b893d5df01ea54eb9c0b90e75d0968
Rakibul islam <rakibcuet16@gmail.com>
2020-01-06 07:55:41 +0600

    recycler adapter updated

--- app/src/main/java/com/algoplay/visualgo/RecyclerAdapter.java
+++ app/src/main/java/com/algoplay/visualgo/RecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.algoplay.visualgo;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,13 +14,18 @@ import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.viewHolder> {
    ArrayList<Algorithm>myalgorithms;
+   OnClickAlog onClickAlog;
+
+    public RecyclerAdapter(ArrayList<Algorithm> myalgorithms, OnClickAlog onClickAlog) {
+        this.myalgorithms = myalgorithms;
+        this.onClickAlog = onClickAlog;
+    }
 
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-
-        return null;
+      View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.homerecycleritem,parent,false);
+       return  new viewHolder(view,onClickAlog);
     }
 
     @Override
@@ -35,13 +41,26 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.viewHo
              return myalgorithms.size();
     }
 
-    public class viewHolder extends RecyclerView.ViewHolder{
+    public class viewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView algoname;
         TextView algocreator;
+        OnClickAlog onClickAlog;
 
-        public viewHolder(@NonNull View itemView) {
+
+        public viewHolder(@NonNull View itemView,OnClickAlog onClickAlog) {
             super(itemView);
+            this.onClickAlog=onClickAlog;
+            itemView.setOnClickListener(this);
 
         }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
+    public interface OnClickAlog{
+        void onclick(Algorithm algorithm);
     }
 }
